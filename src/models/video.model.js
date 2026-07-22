35859ccabb27fa9d99f06cc83f31b207d506e021
Kritagya Shukka <kritagyashukla16@gmail.com>
2026-07-23 01:25:45 +0530

    done

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoschema=new Schema(
+    {
+     videofile:{
+        type:String,//cloudinary url
+        required:true
+
+     },
+     thumbnail:{
+         type:String,
+         required:true
+     },
+        title:{
+         type:String,
+         required:true
+     },
+        description:{
+         type:String,
+         required:true
+     },
+        duration:{
+         type:Number,
+         required:true
+     },
+     views:{
+        type:Number,
+        default:0
+     },
+     ispublished:{
+        type=Boolean,
+        default:true
+     },
+     owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+     }
+
+    },{
+        timestamps:true
+    }
+)
+export const video=mongoose.model("Video",videoschema)
