e03a8440d855b7642d9916521722a4497b64bf01
Kritagya Shukka <kritagyashukla16@gmail.com>
2026-07-23 23:33:07 +0530

    okay

---
+++ src/util/cloudinary.js
@@ -0,0 +1,23 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs";
+cloudinary.config({ 
+        cloud_name: 'hicgcmx4', 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret:process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    
+const uploadOnCloudinary=async (localFilePath) => {
+try{
+    if(!localFilePath) return null
+
+ const response= await cloudinary.uploader.upload(localFilePath,{
+        resource_type="auto"
+    })
+    return response;
+}
+
+catch (error){
+     fs.unlinkSync(localFilePath)//remove the locally saved temprory file as the upload operation got failed
+}}
+
+export {uploadOnCloudinary}
